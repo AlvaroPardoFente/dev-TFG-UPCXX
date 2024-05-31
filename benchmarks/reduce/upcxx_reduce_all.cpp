2d64a51e923b4d8b6a55e3967d5075495fab66cd
Álvaro Pardo Fente <alvaro.pardo.fente@udc.es>
2024-05-31 17:58:18 +0200

    Refactor: Adaptadas operaciones reduce al tamaño de bloque correcto

--- benchmarks/reduce/upcxx_reduce_all.cpp
+++ benchmarks/reduce/upcxx_reduce_all.cpp
@@ -8,40 +8,42 @@ public:
     std::vector<uint32_t> value;
     std::vector<uint32_t> result;
 
+    uint32_t nums_per_rank;
+
     void init(int argc, char *argv[]) override
     {
         UpcxxBenchmarkScheme::init(argc, argv);
 
+        nums_per_rank = number_count;
+
         // Vector initialization
-        value.resize(number_count / world_size);
-        for (uint32_t i = 0; i < number_count / world_size; i++)
+        value.resize(nums_per_rank);
+        for (uint32_t i = 0; i < nums_per_rank; i++)
         {
             value.at(i) = i * world_size + world_rank;
         }
 
         // Result vector
-        result.resize(number_count / world_size);
+        result.resize(nums_per_rank);
     };
 
     void benchmark_body() override
     {
-        upcxx::reduce_all(value.data(), result.data(), number_count / world_size, upcxx::op_fast_add).wait();
+        upcxx::reduce_all(value.data(), result.data(), nums_per_rank, upcxx::op_fast_add).wait();
     }
 
     void reset_result() override
     {
+        // Print result
+        // std::cout << "Result: ";
+        // for (uint32_t i = 0; i < nums_per_rank; i++)
+        // {
+        //     std::cout << result.at(i) << " ";
+        // }
+        // std::cout << std::endl;
+
         if (world_rank == 0)
         {
-            // if (world_rank == 0)
-            // {
-            //     std::cout << "Result: ";
-            //     for (uint32_t i = 0; i < number_count / world_size; i++)
-            //     {
-            //         std::cout << result.at(i) << " ";
-            //     }
-            //     std::cout << std::endl;
-            // }
-
             // Reset result
             std::fill(result.begin(), result.end(), 0);
         }
