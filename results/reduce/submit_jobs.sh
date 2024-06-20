1ecfee2c8d80f3569c896b6ecd83bb4565d84b88
Álvaro Pardo Fente <alvaro.pardo.fente@udc.es>
2024-06-20 09:50:55 +0200

    Tests: Actuializados tests de reduce

--- results/reduce/submit_jobs.sh
+++ results/reduce/submit_jobs.sh
@@ -6,7 +6,7 @@
 NODE_VALUES=(1 2 4 6 8 10 12) # Different amount of nodes
 NTASKS_VALUES=(2 4 8 12 16 20 24)  # Corresponding number of tasks for each node value
 PROGRAM_PATH="$HOME/dev-TFG-UPCXX/build/reduce/"
-PROGRAMS=("mpi_reduce_all" "upcxx_reduce_all") 
+PROGRAMS=("mpi_reduce_one" "upcxx_reduce_one") 
 MEASUREMENT_MODE="max" # root, min, max, avg, all
 
 # Check if NODE_VALUES and NTASKS_VALUES arrays have the same length
@@ -27,10 +27,10 @@ for i in "${!NODE_VALUES[@]}"; do
     # Determine which sbatch script to use based on the program name prefix
     if [[ $PROGRAM == mpi* ]]; then
       SBATCH_SCRIPT="new_mpi_sbatch.sh"
-      sbatch --exclusive -N "$NODES" -n "$NTASKS" --ntasks-per-node=2 -c 1 -t 00:05:00 -p compute0 -o "$OUTPUT_FILE" ~/dev-TFG-UPCXX/"$SBATCH_SCRIPT" --value=sizes.txt "$PROGRAM_PATH""$PROGRAM" --repetitions 100 -m "$MEASUREMENT_MODE" -q
+      sbatch --exclusive -N "$NODES" -n "$NTASKS" --ntasks-per-node=2 -c 1 -t 00:05:00 -p compute0 -o "$OUTPUT_FILE" ~/dev-TFG-UPCXX/"$SBATCH_SCRIPT" --value=sizes.txt "$PROGRAM_PATH""$PROGRAM" --repetitions 1000 -m "$MEASUREMENT_MODE" -q --warmup-repetitions 200
     elif [[ $PROGRAM == upcxx* ]]; then
       SBATCH_SCRIPT="new_upcxx_sbatch.sh"
-      UPCXX_SHARED_HEAP_SIZE=2G sbatch --exclusive -N "$NODES" -n "$NTASKS" --ntasks-per-node=2 -c 1 -t 00:05:00 -p compute0 -o "$OUTPUT_FILE" ~/dev-TFG-UPCXX/"$SBATCH_SCRIPT" --value=sizes.txt "$PROGRAM_PATH""$PROGRAM" --repetitions 100 -m "$MEASUREMENT_MODE" -q
+      UPCXX_SHARED_HEAP_SIZE=2G sbatch --exclusive -N "$NODES" -n "$NTASKS" --ntasks-per-node=2 -c 1 -t 00:05:00 -p compute0 -o "$OUTPUT_FILE" ~/dev-TFG-UPCXX/"$SBATCH_SCRIPT" --value=sizes.txt "$PROGRAM_PATH""$PROGRAM" --repetitions 1000 -m "$MEASUREMENT_MODE" -q --warmup-repetitions 200
     else
       echo "Unknown program prefix for $PROGRAM"
       continue
